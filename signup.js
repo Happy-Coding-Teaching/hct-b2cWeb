ad3118693f1a750d53866b88067a466237042743
debuchan030 <debuchan030@gmail.com>
2023-01-09 22:07:55 +0800

    add pic

--- signup.js
+++ signup.js
@@ -1,36 +1,97 @@
 function chooseClass() {
     var x = document.getElementById("mySelect").value;
     if (x == 1) {
-        var elems = document.querySelectorAll(".all");
+        var elems = document.querySelectorAll("#selectCourse option");
         var length = elems.length;
+        console.log(elems);
         for (var index = 0; index < length; index++) {
-            elems[index].style.display = "";
-        }
-        elems = document.querySelectorAll('.winter');
-        length = elems.length;
-        for (var index = 0; index < length; index++) {
-            elems[index].style.display = "none";
+            console.log(elems[index].className);
+            if (elems[index].className == "APCS") {
+                elems[index].style.display = "";
+            }
+            else {
+                elems[index].style.display = "none";
+            }
         }
     } else if (x == 2) {
-        var elems = document.querySelectorAll(".all");
+        var elems = document.querySelectorAll("#selectCourse option");
         var length = elems.length;
+        console.log(elems);
         for (var index = 0; index < length; index++) {
-            elems[index].style.display = "none";
+            console.log(elems[index].className);
+            if (elems[index].className == "adult") {
+                elems[index].style.display = "";
+            }
+            else {
+                elems[index].style.display = "none";
+            }
         }
-        elems = document.querySelectorAll('.winter');
-        length = elems.length;
+    }
+    else if (x == 3) {
+        var elems = document.querySelectorAll("#selectCourse option");
+        var length = elems.length;
+        console.log(elems);
         for (var index = 0; index < length; index++) {
-            elems[index].style.display = "";
+            console.log(elems[index].className);
+            if (elems[index].className == "junior") {
+                elems[index].style.display = "";
+            }
+            else {
+                elems[index].style.display = "none";
+            }
+        }
+    }
+    else if (x == 4) {
+        var elems = document.querySelectorAll("#selectCourse option");
+        var length = elems.length;
+        console.log(elems);
+        for (var index = 0; index < length; index++) {
+            console.log(elems[index].className);
+            if (elems[index].className == "middle") {
+                elems[index].style.display = "";
+            }
+            else {
+                elems[index].style.display = "none";
+            }
+        }
+    }
+    else if (x == 5) {
+        var elems = document.querySelectorAll("#selectCourse option");
+        var length = elems.length;
+        console.log(elems);
+        for (var index = 0; index < length; index++) {
+            console.log(elems[index].className);
+            if (elems[index].className == "winter") {
+                elems[index].style.display = "";
+            }
+            else {
+                elems[index].style.display = "none";
+            }
         }
     }
 
-    var temp = "選擇課程";
-    var selectCourse = document.getElementById('selectCourse');
+    // var temp = "選擇課程";
+    // var selectCourse = document.getElementById('selectCourse');
 
-    for (var i, j = 0; i = selectCourse.options[j]; j++) {
-        if (i.value == temp) {
-            selectCourse.selectedIndex = j;
-            break;
-        }
-    }
+    // for (var i, j = 0; i = selectCourse.options[j]; j++) {
+    //     if (i.value == temp) {
+    //         selectCourse.selectedIndex = j;
+    //         break;
+    //     }
+    // }
+}
+function chooseCourse() {
+    var x = document.getElementById("selectCourse");
+    console.log(x.options[x.selectedIndex].className);
+
+    var img = document.createElement("img");
+
+    var imgSrc = "../assets/img/DM/"+x.options[x.selectedIndex].className+"/"+x.value+".jpg";
+    console.log(imgSrc);
+    img.src = imgSrc;
+    // img.width = "100%"; 
+    img.style.maxWidth = "1320px";
+    var src = document.getElementById("setPic");
+    src.innerHTML = "";
+    src.appendChild(img);
 }
